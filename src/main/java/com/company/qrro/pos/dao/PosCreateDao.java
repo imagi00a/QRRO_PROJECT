5d045212c4c4576ef3951c8d9a2f6b62da0681fb
imagi00a <144099869+imagi00a@users.noreply.github.com>
2024-01-09 14:52:43 +0900

    Delete src directory

--- src/main/java/com/company/qrro/pos/dao/PosCreateDao.java
+++
@@ -1,9 +0,0 @@
-package com.company.qrro.pos.dao;
-
-import java.util.Map;
-
-public interface PosCreateDao {
-
-	public int create(Map map);
-	
-}
--- src/main/java/com/company/qrro/pos/sql/PosCreateMapper.java
+++
@@ -1,9 +0,0 @@
-package com.company.qrro.pos.sql;
-
-import java.util.Map;
-
-public interface PosCreateMapper {
-
-	public int create(Map map);
-	
-}
